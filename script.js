73ffa03929597cb4db07277f968a0258e41b4bd5
akash mishra <akashmishra31869@gmail.com>
2024-05-13 14:38:35 -0400

    all part of this website i created

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ if(close){
 nav.classList.remove('active')
 })
 };
+
+// start writing script for shop section
+let Maniimg=document.getElementById("mainimg")
+let smimg=document.getElementsByClassName("small-img")
+
+smimg[0].onclick=function(){
+    Maniimg.src =  smimg[0].src;
+}
+smimg[1].onclick=function(){
+    Maniimg.src =  smimg[1].src;
+}
+smimg[2].onclick=function(){
+    Maniimg.src =  smimg[2].src;
+}
+smimg[3].onclick=function(){
+    Maniimg.src =  smimg[3].src;
+}
+
+// end of script for shop section
